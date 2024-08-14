c1d3c8bbdce8c307849c1b09cebfa5519ef7f772
inbal-samucha <inbalsam2014@gmail.com>
2024-08-14 14:40:30 +0300

    change base url in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3000/api'
+
+let BASE_URL;
+
+if (process.env.NODE_ENV === 'production') {
+  BASE_URL = 'https://auth-system-server-vyt0.onrender.com/api';
+} else {
+  BASE_URL = 'http://localhost:3000/api';
+}
+
+console.log('process.env.NODE_ENV ', process.env.NODE_ENV);
+console.log('BASE_URL ', BASE_URL);
+
+
 
 export default axios.create({
   baseURL: BASE_URL
